458e837c40c06d2b6746d1e2cf3c888a2d1a12ca
yeongi <cbg5255@gmail.com>
2022-10-11 13:57:10 +0900

    project init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!!</h1>
+      <p>"이상하다 저기서는 잘 됐는데 ? ㅠㅠ"</p>
+      <p>"왜 글씨가 잘 나오지 않을까??"</p>
     </div>
   );
 }
