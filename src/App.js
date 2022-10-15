dbebe93426accf4da2355a8781faa44290f6f6df
yeongi <cbg5255@gmail.com>
2022-10-15 14:19:53 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,86 @@
+import { Button, Checkbox, Form, Input } from "antd";
+import React from "react";
+
 function App() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <div className="App">
-      <h1>Hello World!!</h1>
-      <p>"이상하다 저기서는 잘 됐는데 ? ㅠㅠ"</p>
-      <p>"왜 글씨가 잘 나오지 않을까??"</p>
+      <h1>Work-Manage with electron</h1>
+      <p>일렉트론 웹 개발 언어로 데스크톱 프로그램을 개발할 수 있음</p>
+      <p>이걸로 다 베포 해서 하나의 서버로 통신할 예정</p>
+      <h1> 페이지 정의 </h1>
+      <div>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 }
