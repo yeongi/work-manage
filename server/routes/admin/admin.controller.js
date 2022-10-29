829146c74b7cee4691072ccba85c00abdb6ceb91
yeongi <cbg5255@gmail.com>
2022-10-29 16:09:25 +0900

    서버. 관리자 api 기능 목록 작성

--- server/routes/admin/admin.controller.js
+++ server/routes/admin/admin.controller.js
@@ -2,17 +2,25 @@ const express = require("express");
 const router = express.Router();
 const AdminService = require("./admin.service");
 
-//employee
-router.post("/login", async (req, res) => {
-  console.log("로그인 처리");
-  const body = req.body;
+// admin 관리자
+// 작성자: yeongi, 장의영
 
-  try {
-    const result = await AdminService.addEmployee(body);
-    console.log(result);
-  } catch (err) {
-    console.log(err);
-  }
-});
+//사원 추가
+router.post("/emp/add", async (req, res) => {});
+
+//선체 추가
+router.post("/hull/add", async (req, res) => {});
+
+//선체 조회
+router.get("/hull/:hullno/view", async (req, res) => {});
+
+//블럭 추가
+router.post("/blk/:hullno/add", async (req, res) => {});
+
+//블럭 시수 조회
+router.get("/blk/:hullno/view", async (req, res) => {});
+
+//블럭 수정
+router.post("/blk/:blkno/edit", async (req, res) => {});
 
 module.exports = router;
