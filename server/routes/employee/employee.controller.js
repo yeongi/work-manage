5a342ca0aabd9a36f17e2dc3cde9aaf659e9a985
yeongi <cbg5255@gmail.com>
2022-10-29 16:05:19 +0900

    서버 emp. api 기능 목록 작성

--- server/routes/employee/employee.controller.js
+++ server/routes/employee/employee.controller.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const empService = require("./employee.service");
 
 //employee
+//작성자: yeongi , 장의영
+
+//로그인
 router.post("/login", async (req, res) => {
   const body = req.body;
   try {
@@ -15,5 +18,26 @@ router.post("/login", async (req, res) => {
     console.log(err);
   }
 });
+
+//선체 리스트 가져 오기
+router.get("/hull/list", async (req, res) => {});
+
+//블럭 리스트 가져 오기
+router.get("/hull/:hullno/block", async (req, res) => {});
+
+//업무 리스트 조회
+router.get("/work/list", async (req, res) => {});
+
+//업무 추가
+router.post("/work/list", async (req, res) => {});
+
+//업무 내역 입력
+router.post("/work/record", async (req, res) => {});
+
+//본인 업무 내역 조회
+router.get("/work/record/:empno", async (req, res) => {});
+
+//업무 내역 입력
+router.post("/work/record/edit/:workno", async (req, res) => {});
 
 module.exports = router;
