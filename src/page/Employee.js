cd780dd73d0568f20ec8d35ea7b1dcf19f0415c6
yeongi <cbg5255@gmail.com>
2022-11-27 19:39:19 +0900

    feat : employee 달력 기능 추가

--- src/page/Employee.js
+++ src/page/Employee.js
@@ -1,14 +1,18 @@
 import LoginState from "../component/emp/LoginState";
+import WorkCalendar from "../component/emp/WorkCalendar";
+import classes from "./Employee.module.css";
 
 const Employee = (props) => {
   return (
     <>
-      <div>
-        <section>
+      <div className={classes["emp-wrapper"]}>
+        <section className={classes["emp-main"]}>
           <LoginState />
+          <h1>데이터 넣기</h1>
         </section>
-        <section>
+        <section className={classes["emp-calendar"]}>
           <h1> 업무 내역 확인</h1>
+          <WorkCalendar />
         </section>
       </div>
     </>
--- src/page/Main.js
+++ src/page/Main.js
@@ -2,16 +2,19 @@ import { Button } from "antd";
 import Admin from "./Admin";
 import Employee from "./Employee";
 import { useLoginCtx } from "../lib/store/LoginContext";
+import classes from "./page.module.css";
 
 const Main = () => {
   const loginCtx = useLoginCtx();
 
   return (
     <>
-      <h1> 메인 페이지 입니다.</h1>
-      {loginCtx.state.IS_ADMIN ? <Admin /> : <Employee />}
+      <div className={classes.main}>
+        <h1> 메인 페이지 입니다.</h1>
 
-      <Button onClick={loginCtx.onLogout}>로그아웃 하기</Button>
+        <Button onClick={loginCtx.onLogout}>로그아웃 하기</Button>
+      </div>
+      {loginCtx.state.IS_ADMIN ? <Admin /> : <Employee />}
     </>
   );
 };
