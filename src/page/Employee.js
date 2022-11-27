85c0891704c8d2b435796e891db27f9ceac4063b
yeongi <cbg5255@gmail.com>
2022-11-27 19:04:31 +0900

    feat : 로그인 상태 기능

--- src/page/Employee.js
+++ src/page/Employee.js
@@ -1,5 +1,18 @@
+import LoginState from "../component/emp/LoginState";
+
 const Employee = (props) => {
-  return <h1>사원 페이지 입니다.</h1>;
+  return (
+    <>
+      <div>
+        <section>
+          <LoginState />
+        </section>
+        <section>
+          <h1> 업무 내역 확인</h1>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Employee;
