3e90cfcec1d8d0b61fd2829a1981796d21074673
yeongi <cbg5255@gmail.com>
2023-02-01 17:04:11 +0900

    feat : excel 내보내기 데이터 프로세싱 구현

--- src/component/admin/sheet/MonthListExport.js
+++ src/component/admin/sheet/MonthListExport.js
@@ -1,5 +1,7 @@
 import { Button } from "antd";
 import * as XLSX from "xlsx";
+import dayjs from "dayjs";
+import { dayJsYMD } from "../../../lib/dayJs";
 
 const MonthListExport = ({ list, fileName }) => {
   const getEmpExcelHandler = () => {
@@ -42,10 +44,48 @@ const listToOneJson = (lists) => {
       SUM_OVER_MH,
     };
     arr.push(myList);
-    arr.push(...WORK_LIST);
-    arr.push(...DATE_LIST);
+    arr.push(
+      ...WORK_LIST.map(
+        ({ SUM_INP_MH, SUM_OVER_MH, WORK_CODE, WORK_DES, WORK_TYPE }) => {
+          return {
+            WORK_DES,
+            WORK_TYPE,
+            WORK_SUM_INP_MH: SUM_INP_MH,
+            WORK_SUM_OVER_MH: SUM_OVER_MH,
+          };
+        }
+      )
+    );
+    arr.push(
+      ...DATE_LIST.map(
+        ({
+          SHIPYARD,
+          HULL_NO,
+          HULL_TYPE,
+          BLK_NO,
+          EMP_NAME,
+          EMP_NO,
+          INP_MH,
+          NORM_MH,
+          OVERTIME_MH,
+          RES_MH,
+          WORK_CODE,
+          WORK_DATE,
+          WORK_DES,
+          WORK_TYPE,
+        }) => {
+          return {
+            EMP_NAME,
+            EMP_NO,
+            INP_MH,
+            OVERTIME_MH,
+            WORK_DATE: dayJsYMD(dayjs(WORK_DATE)),
+            업무: WORK_DES + "/" + WORK_TYPE,
+          };
+        }
+      )
+    );
   });
-  console.log(arr);
 
   return arr;
 };
