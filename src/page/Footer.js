ae763efc1683e9bcf36382a813b983b3eb75ba6a
yeongi <cbg5255@gmail.com>
2023-02-03 23:22:52 +0900

    style : layout 수정

---
+++ src/page/Footer.js
@@ -0,0 +1,12 @@
+import classes from "./Login.module.css";
+
+const Footer = () => {
+  return (
+    <section className={classes["footer-section"]}>
+      <p>제작자 github : yeongi</p>
+      <hr />
+    </section>
+  );
+};
+
+export default Footer;
--- src/page/Main.js
+++ src/page/Main.js
@@ -3,19 +3,29 @@ import Admin from "./Admin";
 import EmpMain from "./EmpMain";
 import { useLoginCtx } from "../lib/store/LoginContext";
 import classes from "./page.module.css";
+import Footer from "./Footer";
 
 const Main = () => {
   const loginCtx = useLoginCtx();
 
   return (
-    <>
-      <div className={classes.main}>
-        <h1> WORK - MANAGE </h1>
-        <Button onClick={loginCtx.onLogout}>로그아웃 하기</Button>
-      </div>
-      <hr />
-      {loginCtx.state.IS_ADMIN ? <Admin /> : <EmpMain />}
-    </>
+    <div className={classes.wrapper}>
+      <section className={classes.main}>
+        <div className={classes["header-section"]}>
+          <div className={classes.logo}>로고 넣을듯</div>
+          <div className={classes.header}>
+            <h1> WORK - MANAGE </h1>
+          </div>
+          <Button className={classes.button} onClick={loginCtx.onLogout}>
+            로그아웃 하기
+          </Button>
+        </div>
+      </section>
+      <section className={classes["body-section"]}>
+        {loginCtx.state.IS_ADMIN ? <Admin /> : <EmpMain />}
+      </section>
+      <Footer />
+    </div>
   );
 };
 
