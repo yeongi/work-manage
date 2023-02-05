bfc2c3900238a6160388a4a6a6d8726e647b8147
yeongi <cbg5255@gmail.com>
2023-02-05 18:08:28 +0900

    style : empmain 페이지 스타일 추가

--- src/component/emp/EmpWorkRecord.js
+++ src/component/emp/EmpWorkRecord.js
@@ -4,7 +4,7 @@ const EmpWorkRecord = ({ EMP_NAME, recordList }) => {
   return (
     <div>
       <h1> {EMP_NAME}님 업무 기록 내역</h1>
-      {recordList.length > 0 && <WorkList list={recordList} />}
+      {recordList.length > 0 && <WorkList myList={recordList} />}
     </div>
   );
 };
--- src/component/emp/WorkList.js
+++ src/component/emp/WorkList.js
@@ -1,74 +1,63 @@
-import { dayJsYMD } from "../../lib/dayJs";
+import { HEADER } from "../../lib/const/List";
+import mappingHeader from "../../lib/MappingHeader";
+import { List } from "antd";
 import dayjs from "dayjs";
-import { v4 } from "uuid";
+import { dayJsYMD } from "../../lib/dayJs";
 import classes from "./WorkList.module.css";
 
-const headers = [
-  "날짜",
-  "선체 번호",
-  "선체 종류",
-  "조선소",
-  "블럭 번호",
-  "투입 시수",
-  "야근 시수",
+const header = HEADER.BLK_LIST;
 
-  "업무 종류",
-  "업무 내용",
-  "사원 이름",
-  "사원 번호",
-];
-
-const WorkList = ({ list }) => {
+const WorkList = ({ myList }) => {
+  console.log(myList);
   return (
     <div className={classes.wrapper}>
-      <section className={classes["hedear-wrapper"]}>
-        {headers.map((header) => {
+      <List
+        size="large"
+        bordered
+        header={
+          <div className={classes.header}>{header.map(mappingHeader)}</div>
+        }
+        footer={<div>제작자 github : yeongi</div>}
+        dataSource={myList}
+        renderItem={({
+          HULL_NO,
+          HULL_TYPE,
+          SHIPYARD,
+          BLK_NO,
+          INP_MH,
+          OVERTIME_MH,
+          WORK_DATE,
+          WORK_DES,
+          WORK_TYPE,
+          EMP_NAME,
+          NORM_MH,
+          EMP_NO,
+        }) => {
           return (
-            <div key={v4()} className={classes["header-item"]}>
-              {header}
-            </div>
-          );
-        })}
-      </section>
-      <section>
-        {list.map(
-          ({
-            HULL_NO,
-            HULL_TYPE,
-            SHIPYARD,
-            BLK_NO,
-            INP_MH,
-            OVERTIME_MH,
-            WORK_DATE,
-            WORK_DES,
-            WORK_TYPE,
-            EMP_NAME,
-            EMP_NO,
-          }) => {
-            return (
-              <div key={v4()} className={classes["list-wrapper"]}>
-                <p className={classes["list-item"]}>
-                  {dayJsYMD(dayjs(WORK_DATE))}
+            <List.Item>
+              <div className={classes["list-wrapper"]}>
+                <p>{SHIPYARD}</p>
+                <p>{BLK_NO}</p>
+                <p>{HULL_NO}</p>
+                <p>{HULL_TYPE}</p>
+
+                <p>{WORK_DES}</p>
+                <p>{WORK_TYPE}</p>
+                <p>{EMP_NAME}</p>
+                <p>{EMP_NO}</p>
+                <p>{NORM_MH}</p>
+                <p>
+                  <b className={classes.MH}>{INP_MH}</b>
                 </p>
-                <p className={classes["list-item"]}>{HULL_NO}</p>
-                <p className={classes["list-item"]}>{HULL_TYPE}</p>
-                <p className={classes["list-item"]}>{SHIPYARD}</p>
-                <p className={classes["list-item"]}>{BLK_NO}</p>
-                <p className={classes["list-item"]}>
-                  <b className={classes["list-item"]}>{INP_MH}</b>
+                <p>
+                  <b className={classes.MH}>{OVERTIME_MH}</b>
                 </p>
-                <p className={classes["list-item"]}>
-                  <b className={classes["list-item"]}>{OVERTIME_MH}</b>
-                </p>
-                <p className={classes["list-item"]}>{WORK_DES}</p>
-                <p className={classes["list-item"]}>{WORK_TYPE}</p>
-                <p className={classes["list-item"]}>{EMP_NAME}</p>
-                <p className={classes["list-item"]}>{EMP_NO}</p>
+                <p>{dayJsYMD(dayjs(WORK_DATE))}</p>
               </div>
-            );
-          }
-        )}
-      </section>
+            </List.Item>
+          );
+        }}
+      />
     </div>
   );
 };
