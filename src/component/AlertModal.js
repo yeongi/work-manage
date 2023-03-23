793a9d5faa3ec55cee1827bfe6d43c7fa8aabbf8
yeongi <cbg5255@gmail.com>
2023-03-23 17:50:03 +0900

    feat : modal 창 구현

---
+++ src/component/AlertModal.js
@@ -0,0 +1,24 @@
+import { Modal } from "antd";
+
+const AlertModal = ({ title, state, setIsModalOpen }) => {
+  const handleOk = () => {
+    state.handler();
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      {state.open && (
+        <Modal title={title} open={state.open} onOk={handleOk} type="success">
+          <p>{state.message}</p>
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default AlertModal;
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,40 +1,50 @@
-import { Button, Checkbox, Form, Input } from "antd";
-import { useEffect, useRef } from "react";
+import { Button, Form, Input } from "antd";
+import { useState, useRef } from "react";
 import empHandler from "../lib/handler/EmpHander";
 import { useLoginCtx } from "../lib/store/LoginContext";
 import classes from "./Login.module.css";
-import { Avatar } from "antd";
-import imgUrl from "../img/logo.png";
+import AlertModal from "../component/AlertModal";
 
 const Login = () => {
   const loginCtx = useLoginCtx();
-
   const inputRef = useRef();
-
-  const focusing = () => inputRef.current.focus({ cursor: "start" });
+  const [modal, setOpen] = useState({ open: false, message: "" });
 
   const onFinish = async (values) => {
     try {
       const result = await empHandler.signIn(values);
       if (result.status === 200) {
-        alert(`로그인 결과 : ${result.message}`);
-        const { EMP_NAME, EMP_NO, ADMIN } = result.data;
-        loginCtx.onLogin({ IS_LOGIN: true, EMP_NO, EMP_NAME, IS_ADMIN: ADMIN });
+        setOpen({
+          open: true,
+          message: "로그인 성공",
+          handler: () => {
+            const { EMP_NAME, EMP_NO, ADMIN } = result.data;
+            loginCtx.onLogin({
+              IS_LOGIN: true,
+              EMP_NO,
+              EMP_NAME,
+              IS_ADMIN: ADMIN,
+            });
+          },
+        });
+      } else {
       }
     } catch (error) {}
   };
 
-  useEffect(() => {
-    focusing();
-  }, []);
-
   const onFinishFailed = (errorInfo) => {
     alert("에러", errorInfo);
-    focusing();
   };
 
   return (
     <div className={classes["login-wrapper"]}>
+      {
+        <AlertModal
+          title="로그인 결과"
+          state={modal}
+          setIsModalOpen={setOpen}
+        />
+      }
       <section className={classes["header-section"]}>
         <div>
           <h1>하나 E&T</h1>
