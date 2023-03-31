86adf86c9e251325488c90c8747ffd7f1ead0772
yeongi <cbg5255@gmail.com>
2023-03-31 17:04:05 +0900

    feat : modal에 정보 뷰 구현 및 업데이트 버튼 구현

--- src/component/admin/hull/HullList.js
+++ src/component/admin/hull/HullList.js
@@ -1,25 +1,23 @@
 import { Button, List } from "antd";
 import classes from "./HullList.module.css";
 import mappingHeader from "../../../lib/MappingHeader";
-import AdminHandler from "../../../lib/handler/AdminHandler";
 import { useState } from "react";
 import HullModal from "./modal/HullModal";
 
+const header = ["SHIP_YARD", "HULL_NO", "HULL_TYPE", "자세히 보기"];
+
 const HullList = ({ hullList, refreshHandler }) => {
-  const header = ["SHIP_YARD", "HULL_NO", "HULL_TYPE", "완료 여부"];
+  const [selectedHull, setHullselect] = useState();
 
-  const [selectedHull, setHullselect] = useState({});
-
-  const completeHandler = async (hullsq) => {
-    const result = await AdminHandler.updateHullComplte(hullsq);
-    alert(result.message);
-    refreshHandler();
+  const resetHullState = async (sq) => {
+    await refreshHandler();
+    await setHullselect(hullList.find(({ HULL_SQ }) => HULL_SQ === sq));
   };
 
   return (
     <>
       <h1>선체 리스트</h1>
-      <HullModal hullInfo={selectedHull} />
+      <HullModal hullInfo={selectedHull} resetHullState={resetHullState} />
       <List
         size="large"
         header={
@@ -27,30 +25,17 @@ const HullList = ({ hullList, refreshHandler }) => {
         }
         footer={<div>제작자 ＠github : yeongi</div>}
         dataSource={hullList}
-        renderItem={({ HULL_NO, HULL_SQ, HULL_TYPE, SHIPYARD, complete }) => {
+        renderItem={(hull) => {
+          const { HULL_NO, HULL_TYPE, SHIPYARD } = hull;
           return (
             <List.Item>
               <div className={classes.wrapper}>
                 <p>{SHIPYARD}</p>
                 <p>{HULL_NO}</p>
                 <p>{HULL_TYPE}</p>
-                <p>{complete === 0 ? "미완료" : "작업 완료"}</p>
-                {/* <Button
-                  onClick={() => {
-                    completeHandler(HULL_SQ);
-                  }}
-                >
-                  업데이트
-                </Button> */}
                 <Button
                   onClick={() => {
-                    setHullselect({
-                      HULL_NO,
-                      HULL_SQ,
-                      HULL_TYPE,
-                      SHIPYARD,
-                      complete,
-                    });
+                    setHullselect(hull);
                   }}
                 >
                   선체 자세히 보기
--- src/component/admin/hull/modal/HullModal.js
+++ src/component/admin/hull/modal/HullModal.js
@@ -1,26 +1,82 @@
 import React, { useEffect, useState } from "react";
-import { Modal } from "antd";
-const HullModal = ({ HULL_NO, HULL_SQ, HULL_TYPE, SHIPYARD, complete }) => {
+import AdminHandler from "../../../../lib/handler/AdminHandler";
+import useModalState from "../../../../lib/state/useMyModal";
+import classes from "./HullModal.module.css";
+import { Modal, Button } from "antd";
+
+const HullModal = ({ hullInfo, resetHullState }) => {
   const [open, setOpen] = useState(false);
+  const { MyModal, openModalFunc } = useModalState("업데이트 완료");
 
   useEffect(() => {
     //hull 정보가 맞으면
-    setOpen(true);
-  }, [HULL_SQ]);
+    if (hullInfo !== undefined) setOpen(true);
+    if (hullInfo === undefined) setOpen(false);
 
-  return (
-    <>
-      <Modal
-        title="선체 정보 조회 및 수정"
-        centered
-        open={open}
-        onOk={() => setOpen(false)}
-        onCancel={() => setOpen(false)}
-        width={900}
-      >
-        <h1>{HULL_TYPE}</h1>
-      </Modal>
-    </>
-  );
+    return () => {};
+  }, [hullInfo]);
+
+  const completeHandler = async (hullsq) => {
+    const result = await AdminHandler.updateHullComplte(hullsq);
+    await resetHullState(hullsq);
+    await openModalFunc(result.message);
+  };
+
+  if (hullInfo !== undefined) {
+    const { HULL_NO, HULL_SQ, HULL_TYPE, SHIPYARD, complete } = hullInfo;
+
+    return (
+      <>
+        <Modal
+          title="선체 정보 조회 및 수정"
+          centered
+          open={open}
+          onOk={() => setOpen(false)}
+          onCancel={() => {
+            resetHullState();
+            setOpen(false);
+          }}
+          width={900}
+        >
+          <section>
+            <h3>선체 정보</h3>
+            <article className={classes["info-wrapper"]}>
+              <div>
+                <p>조선소</p>
+                <hr />
+                <h3>{SHIPYARD}</h3>
+              </div>
+              <div>
+                <p>선체 종류</p>
+                <hr />
+                <h3>{HULL_TYPE}</h3>
+              </div>
+              <div>
+                <p>선체 번호</p>
+                <hr />
+                <h3>{HULL_NO}</h3>
+              </div>
+              <div>
+                <p>작업 완료 여부</p>
+                <hr />
+                <h3>{complete === 0 ? "작업 진행 중" : "작업 완료"}</h3>
+              </div>
+            </article>
+          </section>
+
+          <MyModal />
+          <Button
+            onClick={async () => {
+              await completeHandler(HULL_SQ);
+            }}
+          >
+            업데이트
+          </Button>
+        </Modal>
+      </>
+    );
+  }
+
+  return <h1></h1>;
 };
 export default HullModal;
