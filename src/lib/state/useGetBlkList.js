f37029f90d50a40a09646d87a785367d2e3cb0ab
yeongi <cbg5255@gmail.com>
2023-04-25 23:17:25 +0900

    fix : useEffect 제거

--- src/lib/state/useGetBlkList.js
+++ src/lib/state/useGetBlkList.js
@@ -1,26 +1,21 @@
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import AdminHandler from "../handler/AdminHandler";
 
 const useGetBlkList = () => {
   const [blockList, setBlkList] = useState([]);
   const [hullList, setHullList] = useState([]);
 
-  const getHullList = async () => {
+  const getHullList = useCallback(async () => {
     const serverHullList = await AdminHandler.getHullList();
     serverHullList.reverse();
     serverHullList.pop();
     setHullList(serverHullList.filter((list) => list.complete === 0));
-  };
+  }, []);
 
-  const getBlkList = async (hull_no) => {
+  const getBlkList = useCallback(async (hull_no) => {
     //to do : complete 된 블럭은 안가져 오기
     const blkList = await AdminHandler.getBlkList(hull_no);
     setBlkList(blkList);
-  };
-
-  useEffect(() => {
-    getHullList();
-    getBlkList();
   }, []);
 
   return {
